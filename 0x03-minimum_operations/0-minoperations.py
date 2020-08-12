f85ac962d77024cb2c62ba3651ba97c5d3b0198d
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-08-12 00:00:03 -0700

    Added solution

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,5 +1,43 @@
 #!/usr/bin/python3
-"""Find the minimum number of copy-paste operations needed to make a string."""
+"""
+Provides a function to compute the minimum numper of copy-paste operations
+to produce a given number of characters
+"""
+# pylint: disable=invalid-name
 
-def minOperations(n)
-    """Find the minimum number of copy-paste operations needed to make a string."""
+
+# def sieve_of_eratosthenes(n):
+#     """Compute the sequence of all primes up to n"""
+#     primes = [True for _ in range(2, n + 1)]
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if primes[i - 2] is True:
+#             for j in (i ** 2 + m * i for m in range(n + 1)):
+#                 if j > n:
+#                     break
+#                 primes[j - 2] = False
+#     return [i for i in range(2, n + 1) if primes[i - 2] is True]
+
+
+def sieve_of_eratosthenes(n):
+    """Compute the set of all primes up to n"""
+    primes = set(range(2, n + 1))
+    for i in range(2, int(n ** 0.5) + 1):
+        if i in primes:
+            for j in (i ** 2 + i * m for m in range(n + 1)):
+                if j > n:
+                    break
+                primes.discard(j)
+    return primes
+
+
+def minOperations(n):
+    """Calculates the minimum number of operations to produce n characters"""
+    # primes = sieve_of_eratosthenes(n)
+    ops = 0
+    while n > 1:
+        for prime in range(2, n + 1):
+            if n % prime == 0:
+                ops += prime
+                n //= prime
+                break
+    return ops
