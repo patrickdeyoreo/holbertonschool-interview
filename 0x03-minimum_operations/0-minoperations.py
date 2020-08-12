a966a751638a5cf43e21caa48594601afcecfe9e
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-08-11 23:59:26 -0700

    Adding minoperations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Find the minimum number of copy-paste operations needed to make a string."""
+
+def minOperations(n)
+    """Find the minimum number of copy-paste operations needed to make a string."""
