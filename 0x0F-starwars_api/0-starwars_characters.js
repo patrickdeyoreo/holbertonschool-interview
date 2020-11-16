88dcde278f8acf7e6becae787165c2a9b1ab3f65
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-11-15 23:13:53 -0800

    Fixed semistandard style issues

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 // Query the Starwars API for a list of characters from a given film
 
-const request = require("request");
-const process = require("process");
+const request = require('request');
+const process = require('process');
 
-let url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
     const film = JSON.parse(body);
-    let characters = [];
+    const characters = [];
     for (let i = 0; i < film.characters.length; i++) {
       characters[i] = new Promise((resolve, reject) => {
         request.get(film.characters[i], (error, response, body) => {
