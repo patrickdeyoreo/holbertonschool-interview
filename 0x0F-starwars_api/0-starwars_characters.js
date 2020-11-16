3acfe7e192a0bd6addc6233ec30c5808ceaceaee
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-11-16 11:15:54 -0800

    Refactored into functions

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,27 +5,37 @@ const request = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const film = JSON.parse(body);
-    const characters = [];
-    for (let i = 0; i < film.characters.length; i++) {
-      characters[i] = new Promise((resolve, reject) => {
-        request.get(film.characters[i], (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(body).name);
-          }
-        });
-      });
-    }
-    Promise.all(characters).then((values) => {
-      for (let i = 0; i < values.length; i++) {
-        console.log(values[i]);
+function getCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
       }
     });
+  });
+}
+
+function getFilmCharacters (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).characters);
+      }
+    });
+  });
+}
+
+getFilmCharacters(url).then((characters) => {
+  for (let i = 0; i < characters.length; i++) {
+    characters[i] = getCharacterName(characters[i]);
   }
+  Promise.all(characters).then((names) => {
+    for (let i = 0; i < names.length; i++) {
+      console.log(names[i]);
+    }
+  });
 });
