263a1075f269788286d3d60bb6eb9b5434b424c6
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-12-21 00:19:03 -0800

    Adding non-broken advanced binary since checker is done

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -3,50 +3,49 @@
 #include "search_algos.h"
 
 /**
- * _print_array_slice - print the values in an array
+ * _print_slice - print the values in an array
  *
- * @array: the array of values
- * @lo: the smallest index
- * @hi: the greatest index
+ * @array: array from which to print
+ * @start: lower bound (inclusive)
+ * @stop: upper bound (exclusive)
  */
-static void _print_array_slice(int *array, size_t lo, size_t hi)
+static void _print_slice(int *array, size_t start, size_t stop)
 {
 	printf("Searching in array: ");
-	while (lo <= hi)
+	while (start < stop)
 	{
-		if (lo < hi)
-			printf("%d, ", array[lo++]);
-		else
-			printf("%d\n", array[lo++]);
+		printf("%d", array[start++]);
+		if (start < stop)
+			printf(", ");
 	}
+	printf("\n");
 }
 
 /**
- * _advanced_binary - search for a value in a sorted array of integers
+ * _advanced_binary - search for a value in a sorted array
  *
- * @array: the array of values
- * @lo: the smallest index
- * @hi: the greatest index
- * @value: the value to locate
+ * @array: array in which to search
+ * @start: start bound (inclusive)
+ * @stop: stop bound (exclusive)
+ * @value: value for which to search
  *
- * Return: If value is not present in array or array is NULL, return -1.
- * Otherwise, returh the first index where value is located.
+ * Return: If array is NULL or value is not in array, return -1.
+ * Otherwise, returh the lowest index at which value is located.
  */
-static int _advanced_binary(int *array, size_t lo, size_t hi, int value)
+static int _advanced_binary(int *array, size_t start, size_t stop, int value)
 {
-	size_t mid = (lo + hi) / 2;
+	size_t index = (start + stop - 1) / 2;
 
-	if (lo > hi)
+	if (start >= stop)
 		return (-1);
-
-	_print_array_slice(array, lo, hi);
-	if (array[mid] < value)
-		return (_advanced_binary(array, mid + 1, hi, value));
-	if (array[mid] > value)
-		return (_advanced_binary(array, lo, mid, value));
-	if (array[mid - 1] == value)
-		return (_advanced_binary(array, lo, mid, value));
-	return (mid);
+	_print_slice(array, start, stop);
+	if (array[index] < value)
+		return (_advanced_binary(array, index + 1, stop, value));
+	if (array[index] > value)
+		return (_advanced_binary(array, start, index, value));
+	if (index && array[index - 1] == value)
+		return (_advanced_binary(array, start, index + 1, value));
+	return (index);
 }
 
 /**
@@ -61,6 +60,6 @@ static int _advanced_binary(int *array, size_t lo, size_t hi, int value)
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array && size)
-		return (_advanced_binary(array, 0, size - 1, value));
+		return (_advanced_binary(array, 0, size, value));
 	return (-1);
 }
