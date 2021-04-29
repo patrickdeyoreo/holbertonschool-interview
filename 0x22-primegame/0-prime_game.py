000c2a1c79550d2252b9e7be05f17880f141f02a
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-04-28 21:28:16 -0700

    Testing prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""
-Maria and Ben are playing a game. Given a set of consecutive integers starting
-from 1 up to and including n, they take turns choosing a prime number from the
-set and removing that number and its multiples from the set. The player that
-cannot make a move loses the game.
-"""
+"""Play the prime game."""
+
+
+def get_sieve(n):
+    """Compute a sieve of eratosthenes."""
+    sieve = [True for _ in range(n + 1)]
+    sieve[0] = False
+    sieve[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    return sieve
+
 
 def isWinner(x, nums):
-    """
-    Play the prime game.
-    Arguments:
-        x: number of rounds
-        nums: sequence of n consecutive integers starting at 1
-    Description:
-        It's Maria vs. Ben.
-        Maria always goes first.
-        Both players play optimally.
-    Return:
-        If the winner cannot be determined, return None. Otherwise, return the
-        name of the player that won the most rounds.
-    """
+    """Determine the winner of the prime game."""
     if x < 1:
         return None
 
@@ -31,28 +27,22 @@ def isWinner(x, nums):
     if n < 1:
         return None
 
-    sieve = [True for _ in range(n + 1)]
-    sieve[0] = False
-    sieve[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
+    sieve = get_sieve(n)
 
-    prime_count = [0 for _ in range(len(nums))]
+    prime_counts = [0 for _ in range(len(nums))]
     for i in range(len(nums)):
         count = 0
         for j in range(nums[i] + 1):
             if (sieve[j]):
                 count += 1
-        prime_count[i] = count
+        prime_counts[i] = count
 
     score = 0
-    for num in prime_count:
-        if num % 2 == 0:
+    for count in prime_counts:
+        if count % 2 == 0:
             score += 1
-    if 2 * score > x:
-        return "Ben"
-    if 2 * score < x:
+    if x > 2 * score:
         return "Maria"
+    if x < 2 * score:
+        return "Ben"
     return None
