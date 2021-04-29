34d60de5eab9afc2a650ec4db10176f64de71800
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-04-28 21:07:44 -0700

    Testing old version

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -21,33 +21,28 @@ def isWinner(x, nums):
         If the winner cannot be determined, return None. Otherwise, return the
         name of the player that won the most rounds.
     """
-    if x < 1:
-        return None
-
-    nums = list(nums)
-    if len(nums) == 0:
+    if not nums or x < 1:
         return None
     n = max(nums)
-
     sieve = [True for _ in range(max(n + 1, 2))]
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    sieve[0] = False
-    sieve[1] = False
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            sieve[j] = False
 
-    count = 0
+    sieve[0] = sieve[1] = False
+    c = 0
     for i in range(len(sieve)):
         if sieve[i]:
-            count += 1
-        sieve[i] = count
+            c += 1
+        sieve[i] = c
 
-    score = 0
-    for num in nums:
-        score += sieve[num] % 2 == 1
-    if score * 2 < len(nums):
-        return "Ben"
-    if score * 2 > len(nums):
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
-    return None
+    return "Ben"
